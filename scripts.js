c18f99ff14dd3e4a8e8fd9ed40f2fd542b6703dc
Camila Gomes <camilagomesbsi>
2022-11-26 19:55:40 -0300

    Ultra Lims Project | Script aprimorado e finalizado

--- scripts.js
+++ scripts.js
@@ -2,51 +2,56 @@ let tabela = document.getElementById("tabela");
 let inputCEP = document.getElementById("inputCEP");
 let buttonEnviar = document.getElementById("buttonEnviar");
 let listaTabela = []
+let storage = window.localStorage.getItem("CEPPesquisados")
+let arrayDeCEPS = []
 
+window.onload = function () {
+    if (storage) {
+        if (JSON.parse(storage).length > 0) {
+            arrayDeCEPS = JSON.parse(storage)
+            JSON.parse(storage).forEach(objetoDaVez => {
+                randerizarTabela(objetoDaVez)
+            });
+        }
+    }
+}
 
-function enviarDados(){
+function enviarDados() {
     let url = "https://viacep.com.br/ws/" + inputCEP.value + "/json/"
     fetch(url, {
         method: "GET"
     }).then((response) => {
+
         return response.text()
     }).then((text) => {
-        console.log(text)
-        let object = JSON.parse(text)
-        let row = tabela.insertRow()
-
-        let CEP = row.insertCell(0)
-        CEP.innerHTML = object.cep
-
-        let logradouro = row.insertCell(1)
-        logradouro.innerHTML = object.logradouro
-
-        let complemento = row.insertCell(2)
-        complemento.innerHTML = object.complemento
-
-        let bairro = row.insertCell(3)
-        bairro.innerHTML = object.bairro
-
-        let localidade = row.insertCell(4)
-        localidade.innerHTML = object.localidade
-
-        let uf = row.insertCell(5)
-        uf.innerHTML = object.uf
-
-        let ibge = row.insertCell(6)
-        ibge.innerHTML = object.ibge
-
-        let gia = row.insertCell(7)
-        gia.innerHTML = object.gia
-
-        let ddd = row.insertCell(8)
-        ddd.innerHTML = object.ddd
-
-        let siafi = row.insertCell(9)
-        siafi.innerHTML = object.siafi
+        arrayDeCEPS.push(text)
+        window.localStorage.setItem("CEPPesquisados", JSON.stringify(arrayDeCEPS))
+        console.log(JSON.parse(window.localStorage.getItem("CEPPesquisados")))
+        randerizarTabela(text)
     })
-
 }
-    // listaTabela.push(inputCEP.value)
-    // tabela.innerHTML = listaTabela
-    // alert(tabela.innerHTML)
+
+function randerizarTabela(text) {
+    let object = JSON.parse(text)
+    let row = tabela.insertRow()
+    let CEP = row.insertCell(0)
+    CEP.innerHTML = object.cep
+    let logradouro = row.insertCell(1)
+    logradouro.innerHTML = object.logradouro
+    let complemento = row.insertCell(2)
+    complemento.innerHTML = object.complemento
+    let bairro = row.insertCell(3)
+    bairro.innerHTML = object.bairro
+    let localidade = row.insertCell(4)
+    localidade.innerHTML = object.localidade
+    let uf = row.insertCell(5)
+    uf.innerHTML = object.uf
+    let ibge = row.insertCell(6)
+    ibge.innerHTML = object.ibge
+    let gia = row.insertCell(7)
+    gia.innerHTML = object.gia
+    let ddd = row.insertCell(8)
+    ddd.innerHTML = object.ddd
+    let siafi = row.insertCell(9)
+    siafi.innerHTML = object.siafi
+}
